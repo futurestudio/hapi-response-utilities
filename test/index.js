821e3bafeb0086cc46b0a7d4362a45655e9816de
Marcus Poehls <marcus.poehls@gmail.com>
2018-12-11 16:32:50 +0100

    wip idea

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 'use strict'
 
+const Fs = require('fs')
 const Lab = require('lab')
 const Code = require('code')
 const Hapi = require('hapi')
+const Path = require('path')
 
 let server
 
@@ -80,6 +82,27 @@ experiment('hapi-response-utilities plugin', () => {
     expect(response.statusCode).to.equal(200)
     expect(response.headers['content-type']).to.equal('application/pdf')
     expect(response.headers['content-disposition']).to.contain('hapi-response-utilities')
+    expect(response.payload).to.exist()
+  })
+
+  it('tests the h.png decoration', async () => {
+    const file = Fs.readFileSync(Path.resolve(__dirname, 'fixtures', 'futurestudio.png'))
+
+    server.route({
+      path: '/',
+      method: 'GET',
+      handler: (_, h) => h.png(file)
+    })
+
+    const request = {
+      url: '/',
+      method: 'GET'
+    }
+
+    const response = await server.inject(request)
+    expect(response.statusCode).to.equal(200)
+    expect(response.headers['content-type']).to.equal('image/png')
+    expect(response.headers['content-disposition']).to.contain('inline')
     expect(response.payload).to.exist()
   })
 })
